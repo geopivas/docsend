701f83effc41931d39a3aa7f5f7945de9a1e96e7
geopivas <42982981+geopivas@users.noreply.github.com>
2018-09-25 20:54:59 -0300

    Add files via upload
    Novo método para criar Base64

--- src/main/java/br/gov/arsesp/assinador/utils/FileUtils.java
+++ src/main/java/br/gov/arsesp/assinador/utils/FileUtils.java
@@ -2,20 +2,27 @@ package br.gov.arsesp.assinador.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import com.sun.jersey.core.util.Base64;
+
 public class FileUtils {
 
-	public static byte[] getBytesDoDocumento(String nomeArquivo) throws Exception {		
-		byte[] docEmBytes = null;
+	public static byte[] getBytesDoDocumento(String nomeArquivo) throws Exception {
 		URL systemResource = ClassLoader.getSystemResource(nomeArquivo);
 		if (systemResource == null) {
 			throw new IllegalStateException("Arquivo não localizaddo: " + nomeArquivo);
 		}
 		File file = new File(systemResource.getFile());
+		return getArrayDeBytesDoFile(file);
+	}
+
+	public static byte[] getArrayDeBytesDoFile(File file) throws FileNotFoundException, IOException {
 		InputStream input = new FileInputStream(file);
-		docEmBytes = new byte[input.available()];
+		byte[] docEmBytes = new byte[input.available()];
 		input.read(docEmBytes);
 		input.close();
 		return docEmBytes;
@@ -34,6 +41,16 @@ public class FileUtils {
 		}
 		File file = new File(systemResource.getFile());
 		return file;
+	}
+	
+	public static String getBase64DoArquivo(File arquivo) throws FileNotFoundException, IOException {
+		byte[] arrayDeBytesDoFile = getArrayDeBytesDoFile(arquivo);
+		return getBase64DoArrayDeBytes(arrayDeBytesDoFile);
+	}
+
+	public static String getBase64DoArrayDeBytes(byte[] arrayDeBytesDoFile) {
+		byte[] encode = Base64.encode(arrayDeBytesDoFile);
+		return new String(encode);
 	}
 	
 	
